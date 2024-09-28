dc8e693a95d7a0253916c9d154f04fcbb2293d69
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-09-28 15:40:19 -0700

    add bootstrap container

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -41,73 +41,74 @@ function App() {
     <header className="d-flex justify-content-center align-items-center">
        <h2>React Weather App</h2>
     </header>
+    <div className="container">
+      <div className="card mt-3 mx-auto">
+        { /* Is it true data coming in from open weather based on input location */}
+        {apiData.main? (
+          <div class="card-body text-center">
+            <img
+              src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
+              alt="weather status icon"
+              className="weather-icon"
+            />
+          <p className="h2">
+            {kelvinToFarenheit(apiData.main.temp)}&deg; F
+          </p>
 
-    <div className="card mt-3 mx-auto">
-      { /* Is it true data coming in from open weather based on input location */}
-      {apiData.main? (
-        <div class="card-body text-center">
-          <img
-            src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
-            alt="weather status icon"
-            className="weather-icon"
-          />
-        <p className="h2">
-          {kelvinToFarenheit(apiData.main.temp)}&deg; F
-        </p>
+          <p className="h5">
+            <FontAwesomeIcon
+            icon={faMapMarkerAlt}
+            className="fas fa-1x mr-2 text-dark"
+            />{''}
+            <strong>{apiData.name}</strong>
+          </p>
 
-        <p className="h5">
-          <FontAwesomeIcon
-          icon={faMapMarkerAlt}
-          className="fas fa-1x mr-2 text-dark"
-          />{''}
-          <strong>{apiData.name}</strong>
-        </p>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <p>
+                <FontAwesomeIcon
+                  icon={faTemperatureLow}
+                  className="fas fa-1x mr-2 text-primary"
+                />{''}
+                <strong>
+                {kelvinToFarenheit(apiData.main.temp_min)}&deg; F
+                </strong>
+              </p>
 
-        <div className="row mt-4">
-          <div className="col-md-6">
-            <p>
-              <FontAwesomeIcon
-                icon={faTemperatureLow}
-                className="fas fa-1x mr-2 text-primary"
-              />{''}
-              <strong>
-              {kelvinToFarenheit(apiData.main.temp_min)}&deg; F
-              </strong>
-            </p>
+              <p>
+                <FontAwesomeIcon
+                  icon={faTemperatureHigh}
+                  className="fas fa-1x mr-2 text-danger"
+                />{''}
+                <strong>
+                {kelvinToFarenheit(apiData.main.temp_min)}&deg; F
+                </strong>
+              </p>
+            </div>
 
-            <p>
-              <FontAwesomeIcon
-                icon={faTemperatureHigh}
-                className="fas fa-1x mr-2 text-danger"
-              />{''}
-              <strong>
-              {kelvinToFarenheit(apiData.main.temp_min)}&deg; F
-              </strong>
-            </p>
+            <div className="col-md-6">
+              <p>
+                {''}
+                <strong>{apiData.weather[0].main}</strong>
+              </p>
+
+              <p>
+                <strong>
+                  {' '}
+                  {countries.getName(apiData.sys.country, 'en', {
+                    select: 'official',
+                  })}
+                </strong>
+              </p>
           </div>
+      </div>
+    </div>    
 
-          <div className="col-md-6">
-            <p>
-              {''}
-              <strong>{apiData.weather[0].main}</strong>
-            </p>
+        ) /* Load data if true */
 
-            <p>
-              <strong>
-                {' '}
-                {countries.getName(apiData.sys.country, 'en', {
-                  select: 'official',
-                })}
-              </strong>
-            </p>
-        </div>
-     </div>
-  </div>    
-
-      ) /* Load data if true */
-
-     : (<h1>Loading...</h1>
-      )} {/*Outout message if false */} 
+      : (<h1>Loading...</h1>
+        )} {/*Outout message if false */} 
+      </div>
     </div>
 
     <footer className="footer">
